2c0950031bfb0671054f576ce1960931fb76c563
Travis <Travisang40@gmail.com>
2025-01-30 14:15:13 +0800

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import QueryProvider from "@/components/QueryClientProvider";
 import Footer from "@/components/Footer";
-import Head from "next/head";
+
 
 const jockeyOne = Jockey_One({
   weight :["400"],
@@ -23,8 +23,27 @@ const jua = Jua({
 export const metadata: Metadata = {
   title: "My cook pal",
   description: "Coded and designed by Travis Ang",
-  keywords : ["NextJs" , "TypeScript","JavaScript"]
+  keywords : ["NextJs" , "TypeScript","JavaScript"], 
+  metadataBase : new URL("https://my-cook-pal.vercel.app"), 
+  twitter : { 
+    card: "summary_large_image", 
+    site : "https://my-cook-pal.vercel.app", 
+    creator : "@TravisAng", 
+    title : "My Cook Pal - Recipe App", 
+    description : "Find a recipe that suits ur taste buds", 
+    images: ["feature.png"]
+  }, 
+  openGraph : { 
+    title : "My Cook Pal - Recipe App", 
+    description : "Find a recipe that suits ur taste buds", 
+    url : "https://my-cook-pal.vercel.app",
+    siteName :"My Cook Pal",
+    images: ["feature.png"], 
 
+  }
+
+
+  
 };
 
 export default function RootLayout({
@@ -35,9 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <Head>
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
+     
       <body
         className={`${jockeyOne.variable} ${jua.variable} font-jua antialiased`}
       >
